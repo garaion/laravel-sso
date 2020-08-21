eb303018cdc743ecbf96357093ef4bf7931ac31a
garaion <eze.ivancovsky@gmail.com>
2020-08-21 14:51:07 +0800

    Update LaravelSSOBroker.php
    add new Laravel String

--- src/LaravelSSOBroker.php
+++ src/LaravelSSOBroker.php
@@ -7,6 +7,8 @@ use Zefy\LaravelSSO\Exceptions\MissingConfigurationException;
 use Zefy\SimpleSSO\SSOBroker;
 use GuzzleHttp;
 
+use Illuminate\Support\Str;
+
 /**
  * Class SSOBroker. This class is only a skeleton.
  * First of all, you need to implement abstract functions in your own class.
@@ -68,7 +70,8 @@ class LaravelSSOBroker extends SSOBroker
         }
 
         // If cookie token doesn't exist, we need to create it with unique token...
-        $this->token = str_random(40);
+        
+        $this->token = Str::random(40);
         Cookie::queue(Cookie::make($this->getCookieName(), $this->token, 60));
 
         // ... and attach it to broker session in SSO server.
